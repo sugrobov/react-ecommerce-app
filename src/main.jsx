3c09aa74a21bfad38d3a7a48aeb68d7f7c3d8c58
nikolo <n.sugrobov@mail.ru>
2025-10-22 11:21:36 +0300

    feat(main.jsx): integrate Redux store provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './store'
 import './index.css'
 import App from './containers/App'
 
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>    
   </StrictMode>,
 )
