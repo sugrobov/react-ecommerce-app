53d054ad2731dc983da4f3d4d9c64605388e1d43
nikolo <n.sugrobov@mail.ru>
2026-01-14 13:50:29 +0300

    feat: еализовать персистентность корзины с localForage

--- client/src/store/cartSlice.js
+++ client/src/store/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { saveCart } from "../services/cartStorage";
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -22,6 +23,8 @@ const cartSlice = createSlice({
             //  обновляем сумму
             state.totalAmount = state.items.reduce((total, item) => total + item.price * item.quantity, 0)
 
+            // Сохраняем корзину в локальное хранилище
+            saveCart({ items: state.items, totalAmount: state.totalAmount });
         },
         removeItemFromCart: (state, action) => {
             const id = action.payload;
@@ -29,6 +32,8 @@ const cartSlice = createSlice({
             //  обновляем сумму
             state.totalAmount = state.items.reduce((total, item) => total + item.price * item.quantity, 0);
 
+            // Сохраняем корзину в локальное хранилище
+            saveCart({ items: state.items, totalAmount: state.totalAmount });
         },
         updateItemQuantity: (state, action) => {
             const { id, quantity } = action.payload;
@@ -44,14 +49,25 @@ const cartSlice = createSlice({
             //  обновляем сумму
             state.totalAmount = state.items.reduce((total, item) => total + item.price * item.quantity, 0);
 
+            // Сохраняем корзину в локальное хранилище
+            saveCart({ items: state.items, totalAmount: state.totalAmount });
         },
         clearCart: (state) => {
             state.items = [];
             state.totalAmount = 0
+
+            // Очищаем корзину в локальном хранилище
+            saveCart({ items: [], totalAmount: 0 });
+        },
+        // Новый редьюсер для загрузки корзины из локального хранилища
+        loadCartFromStorage: (state, action) => {
+            const { items, totalAmount } = action.payload;
+            state.items = items || [];
+            state.totalAmount = totalAmount || 0;
         }
     }
 
 });
 
-export const { addItemToCart, removeItemFromCart, updateItemQuantity, clearCart } = cartSlice.actions;
+export const { addItemToCart, removeItemFromCart, updateItemQuantity, clearCart, loadCartFromStorage } = cartSlice.actions;
 export default cartSlice.reducer;
